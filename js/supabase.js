c9f37166d45bea0b9151a6e9c7cdc4325def763f
kiw <106908925+Ikiw97@users.noreply.github.com>
2026-03-12 17:26:49 +0700

    Update supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -4,7 +4,7 @@
 
 // GANTI DUA BARIS DI BAWAH INI DENGAN KREDENSIAL SUPABASE-MU!
 const SUPABASE_URL = 'https://xtiryhqorgvqksqnggnj.supabase.co';
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inh0aXJ5aHFvcmd2cWtzcW5nZ25qIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzMxOTA4MjAsImV4cCI6MjA4ODc2NjgyMH0.tUc12OP0qKqn95HOF53_UO080VN_58Zr0dZZ6yZFndk';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inh0aXJ5aHFvcmd2cWtzcW5nZ25qIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc3MzE5MDgyMCwiZXhwIjoyMDg4NzY2ODIwfQ.H7CUZTk1glB6-2y2BwknysM8nStPjprBjvmlWaYYL9E';
 
 // Inisialisasi Supabase Client
 window.supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
